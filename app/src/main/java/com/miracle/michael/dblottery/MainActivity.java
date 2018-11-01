e17d45a4682b04458fe2400ba8afb5177abcbe05
Administrator <3939@qq.com>
2018-11-01 19:25:47 +0800

    ttt1

--- app/src/main/java/com/miracle/michael/dblottery/MainActivity.java
+++ app/src/main/java/com/miracle/michael/dblottery/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//test1  test2
+        String s1 = "1123213453534234324545321";
     }
 }
