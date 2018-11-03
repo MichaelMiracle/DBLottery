050d0f9d3233876bd5571375f5569d9651e55127
maybe123456 <949397228@qq.com>
2018-11-03 15:10:49 +0800

    shouchang

--- app/src/main/java/com/miracle/sport/me/activity/MyCollectionsActivity.java
+++ app/src/main/java/com/miracle/sport/me/activity/MyCollectionsActivity.java
@@ -9,7 +9,10 @@ import com.miracle.R;
 import com.miracle.base.AppConfig;
 import com.miracle.base.BaseActivity;
 import com.miracle.base.network.PageLoadCallback;
+import com.miracle.base.network.RequestUtil;
 import com.miracle.base.network.ZClient;
+import com.miracle.base.network.ZPageLoadCallback;
+import com.miracle.base.network.ZResponse;
 import com.miracle.base.network.ZService;
 import com.miracle.databinding.SwipeRecyclerBinding;
 import com.miracle.michael.doudizhu.activity.DDZNewsDetailActivity;
@@ -18,12 +21,15 @@ import com.miracle.sport.SportService;
 import com.miracle.sport.home.activity.SimpleWebActivity;
 import com.miracle.sport.home.activity.SimpleWebCommentActivity;
 import com.miracle.sport.home.adapter.HomeListAdapter;
+import com.miracle.sport.home.bean.Football;
 import com.miracle.sport.me.adapter.CollectionsListAdapter;
+
+import java.util.List;
 
 public class MyCollectionsActivity extends BaseActivity<SwipeRecyclerBinding> {
 
     private CollectionsListAdapter mAdapter;
-    private PageLoadCallback callBack;
+    private ZPageLoadCallback callBack;
 
     @Override
     public int getLayout() {
@@ -40,12 +46,15 @@ public class MyCollectionsActivity extends BaseActivity<SwipeRecyclerBinding> {
     }
 
     private void initCallback() {
-        callBack = new PageLoadCallback(mAdapter, binding.recyclerView,loadingDialog) {
+        callBack = new ZPageLoadCallback<ZResponse<List<Football>>>(mAdapter, binding.recyclerView) {
             @Override
             public void requestAction(int page, int limit) {
-                ZClient.getService(SportService.class).getMycollections(page,limit).enqueue(callBack);
+//                ZClient.getService(SportService.class).getMycollections(page,limit).enqueue(callBack);
+                RequestUtil.cacheUpdate(ZClient.getService(SportService.class).getMycollections(page,limit),callBack);
             }
         };
+        callBack.setCachKey("LotteryMyCollections");
+        callBack.setDialog(loadingDialog);
         callBack.initSwipeRefreshLayout(binding.swipeRefreshLayout);
     }
 
