eeba2951e785cd23f3abd6523a55408ed1a87fc4
maybe123456 <949397228@qq.com>
2018-11-05 15:33:12 +0800

    me fragment

--- app/src/main/java/com/miracle/michael/common/activity/MeInfoActivity.java
+++ app/src/main/java/com/miracle/michael/common/activity/MeInfoActivity.java
@@ -48,7 +48,7 @@ public class MeInfoActivity extends BaseActivity<ActivityMeInfoBinding> {
 
     @Override
     public void initView() {
-        setTitle("修改信息");
+        setTitle("修改头像");
         userInfo = (UserInfoBean) getIntent().getSerializableExtra(Constant.USER_INFO);
         GlideApp.with(this).load(userInfo.getImg()).placeholder(R.mipmap.default_head).into(binding.ivHeadImg);
         binding.ibNickName.setText(userInfo.getNickname());
